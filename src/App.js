5c12730050f51c7ecd5ed5e31ef6da21de59ae65
Your Name <you@example.com>
2018-02-01 13:45:36 +0100

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,81 @@ class App extends React.Component {
         super();
         this.state = {
             seriesList: [],
-            seriesEpisodesList: []
+            seriesEpisodesList: [],
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+
+    componentDidMount() {
+
+        fetch('seriesList.json',{})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesList: seriesListDepuisFichier});
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+                alert("j'ai fait ce que j'ai pu");
+            });
+
+        fetch('seriesEpisodesList.json',{})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesEpisodesList: seriesListDepuisFichier});
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+                alert("j'ai fait ce que j'ai pu");
+            });
+    }
+
+    render() {
+        console.log(this.state.seriesList.id);
+        console.log(this.state.seriesList[0]);
+        return (
+            <div>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <ul>
+                    {this.state.value !== "" ?
+                        this.state.seriesList.filter(
+                            sn => sn.seriesName.toLowerCase().trim().indexOf(this.state.value) > -1
+                        ).map(item => <li key={item.id}>{item.seriesName}
+                            <ul>
+                                {/*this.state.seriesEpisodesList.length ?*/
+                                    this.state.seriesEpisodesList.filter(
+                                        si => si.serie_id == item.id
+                                    ).map(episode => episode.episodes_list.filter(
+                                        en => en.episodeName
+                                        ).map(name => <li>{name.episodeName}</li>)
+                                    )
+                                }
+                            </ul>
+                        </li>)
+                        : <li>Loading...</li>
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
+
+/*
         };
     }
 
@@ -45,4 +119,4 @@ class App extends React.Component {
 }
 
 
-export default App;
+export default App;*/
